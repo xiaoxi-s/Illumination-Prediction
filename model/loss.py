0d5f7d9757dcba930373e9fb35a6313f2cf4ff42
adrian <77871407@qq.com>
2020-11-25 16:02:46 -0600

    separate loss function and acc metric

--- model/loss.py
+++ model/loss.py
@@ -1,32 +1,10 @@
 import torch
 
-def average_difference_loss(output, label):
+def average_difference_loss(output, label, weights):
     '''
     Simple averaged squared loss between labels
     '''
-    loss = torch.mean((label - output)**2)
+    test = (label - output)*weights
+    loss = torch.mean(((label - output)*weights)**2)
 
     return loss
-
-
-def average_location_difference(output, label):
-    '''
-    Only calculate location difference
-    '''
-    loss = torch.mean((label[:, :, 0:2] - output[:, :, 0:2])**2)
-
-    return loss
-
-def location_success_count(output, label, threshold=100):
-    '''
-    Simple location accuracy calculator based on distance
-    '''
-    output_locations = output[:, :, 0:2]
-    label_locations = label[:, :, 0:2]
-
-    distance = torch.sqrt(torch.sum((output_locations - label_locations)**2, axis = 2))
-    
-    count = torch.count_nonzero(distance < 100)
-
-    return count
-
---
+++ model/metric.py
@@ -0,0 +1,26 @@
+import torch
+
+
+def location_success_count(output, label, threshold=100):
+    '''
+    Simple location accuracy calculator based on distance
+    '''
+    output_locations = output[:, :, 0:2]
+    label_locations = label[:, :, 0:2]
+
+    distance = torch.sqrt(torch.sum((output_locations - label_locations)**2, axis = 2))
+    
+    count = torch.count_nonzero(distance < threshold)
+
+    return count
+
+
+def color_success_count(output, label, threshold = 100):
+
+    output_colors = output[:, :, -4:-1]
+    label_colors = label[:, :, -4:-1]
+
+    distance = torch.sqrt(torch.sum((output_colors - label_colors)**2, axis = 2))
+    count = torch.count_nonzero(distance < threshold)
+
+    return count
