df3e638aed8c6759d84e94e9f9ed9a8d02061a69
adrian <77871407@qq.com>
2020-11-28 01:14:26 -0600

    bug fixed

--- engine.py
+++ engine.py
@@ -2,13 +2,15 @@
 Interface for training and generating plots
 '''
 
-import argparse
 import os
+import torch
+import argparse
 import matplotlib.pyplot as plt
 import torch.optim as optim
 
 import dataset.transformer as transformer
 
+from datetime import datetime
 from torch.utils.data import DataLoader
 from torch.optim import lr_scheduler
 from torchvision import transforms
@@ -33,6 +35,9 @@ if __name__ == '__main__':
     parser.add_argument('-w', '--width', type=int, help = 'width of the input image', default=900)
     parser.add_argument('-bs', '--batchsize', type=int, help='batch size', default=16)
     parser.add_argument('-epoch', '--epoch', type=int, help='training epoch', default = 50)
+    parser.add_argument('-ag1', '--augmentation_row_num', type=int, help='number of rows for data augmentation', default=1)
+    parser.add_argument('-ag2', '--augmentation_col_num', type=int, help='number of columns for data augmentation', default=1)
+    
     args = parser.parse_args()
 
     # args
@@ -41,6 +46,7 @@ if __name__ == '__main__':
         raise TypeError('Optimizer Must be SGD or Adam')
     
     batch_size = args.batchsize
+    epoch = args.epoch
 
     learning_rate = args.learningrate
     sgd_momentum = args.momentum
@@ -52,18 +58,20 @@ if __name__ == '__main__':
 
     height = args.height
     width = args.width
+
+    augmentation_param = (args.augmentation_row_num, args.augmentation_col_num)
     
     # dataset
     train_ds = EnvironmentJPGDataset(os.path.join('data', 'train_feature_matrix.npy'), os.path.join('data', 'train_label.npy'),\
         transform= transforms.Compose([
                                        transformer.ToTensor(),
                                        transformer.CustomNormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-                                       , augmentation=(1, 2))
+                                       , augmentation=augmentation_param)
     test_ds = EnvironmentJPGDataset(os.path.join('data', 'test_feature_matrix.npy'), os.path.join('data', 'test_label.npy'),\
         transform= transforms.Compose([
                                        transformer.ToTensor(),
                                        transformer.CustomNormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-                                       , augmentation=(1, 2))
+                                       , augmentation=augmentation_param)
     train_dataloader = DataLoader(train_ds, batch_size)
     test_dataloader = DataLoader(test_ds, batch_size)
 
@@ -78,10 +86,15 @@ if __name__ == '__main__':
         optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=adam_beta, eps=adam_epsilon)
 
     scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.01)
-    
+    weights = [20, 20, 0, 1, 1, 1, 1, 1, 1]
+    weights = torch.nn.functional.normalize(torch.Tensor(weights), dim = 0)
     # train
     model, train_loss_epoch, train_acc_epoch, val_loss_epoch, val_acc_epoch = train_model(
         model, average_difference_loss, location_success_count, color_success_count, \
-        optimizer, scheduler, train_dataloader, test_dataloader, 10)
+        optimizer, scheduler, train_dataloader, test_dataloader, weights, epoch)
 
     plot_loss_acc(train_loss_epoch, train_acc_epoch, val_loss_epoch, val_acc_epoch)
+
+    if epoch > 1:
+        torch.save(model, os.path.join('checkpoint', datetime.now().strftime("_%d-%m-%Y_%H_%M_%S") + 'model'))
+
