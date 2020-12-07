bc314eb985b564bffa34277686d0cf5a78021f7c
adrian <77871407@qq.com>
2020-12-07 02:11:41 -0600

    patch dataset with npz

--- patch_dataset.py
+++ patch_dataset.py
@@ -2,47 +2,25 @@ import os
 import argparse
 import numpy as np
 
+from utils import patch_data_from_npz
+from utils import 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='patch all training data together')
     parser.add_argument('-fp', '--folder_path', help='path to the folder that contains npz files')
     parser.add_argument('-dp', '--dest_path', help='output path')
+    parser.add_argument('-fmt', '--file_format', help='file format to be patched', default='npz')
     args = parser.parse_args()
 
     folder_path = args.folder_path
     dest_path = args.dest_path
-
-    npz_names = os.listdir(os.path.join(folder_path))
-    npz_names.sort()
-
-    train_matrix = None
-    train_labels = None
-
-    test_matrix = None
-    test_labels = None
-
-    for npz in npz_names:
-
-        if os.path.isdir(os.path.join(folder_path, npz)):
-            continue
-
-        dataset_dict = np.load(os.path.join(folder_path, npz))
-
-        if train_matrix is None:
-            train_matrix = dataset_dict['train_matrix']
-            train_labels = dataset_dict['train_labels']
-
-            test_matrix = dataset_dict['test_matrix']
-            test_labels = dataset_dict['test_labels']
-        else:
-            train_matrix = np.concatenate((train_matrix, dataset_dict['train_matrix']), axis=0)
-            train_labels = np.concatenate((train_labels, dataset_dict['train_labels']), axis=0)
-
-            test_matrix = np.concatenate((test_matrix, dataset_dict['test_matrix']), axis=0)
-            test_labels = np.concatenate((test_labels, dataset_dict['test_labels']), axis=0)
     
-    np.save(os.path.join(dest_path, 'train_feature_matrix.npy'), train_matrix, allow_pickle=True)
-    np.save(os.path.join(dest_path, 'train_label.npy'), train_labels, allow_pickle=True)
+    file_format = args.file_format
 
-    np.save(os.path.join(dest_path, 'test_feature_matrix.npy'), test_matrix, allow_pickle=True)
-    np.save(os.path.join(dest_path, 'test_label.npy'), test_labels, allow_pickle=True)
-    
+    if file_format == 'npz':
+        patch_data_from_npz(folder_path, dest_path)
+    elif file_format == 'npy':
+        # not implemented. Do this mannually
+        patch_data_from_npy(folder_path, dest_path)
+    else:
+        raise TypeError('Tpye must be npz or npy')
--- utils.py
+++ utils.py
@@ -77,3 +77,44 @@ def load_model(model_path_with_name):
     model = torch.load(model_path_with_name)
 
     return model
+
+
+def patch_data_from_npz(folder_path, dest_path):
+    npz_names = os.listdir(os.path.join(folder_path))
+    npz_names.sort()
+
+    train_matrix = None
+    train_labels = None
+
+    test_matrix = None
+    test_labels = None
+
+    for npz in npz_names:
+
+        if os.path.isdir(os.path.join(folder_path, npz)):
+            continue
+
+        dataset_dict = np.load(os.path.join(folder_path, npz))
+
+        if train_matrix is None:
+            train_matrix = dataset_dict['train_matrix']
+            train_labels = dataset_dict['train_labels']
+
+            test_matrix = dataset_dict['test_matrix']
+            test_labels = dataset_dict['test_labels']
+        else:
+            train_matrix = np.concatenate((train_matrix, dataset_dict['train_matrix']), axis=0)
+            train_labels = np.concatenate((train_labels, dataset_dict['train_labels']), axis=0)
+
+            test_matrix = np.concatenate((test_matrix, dataset_dict['test_matrix']), axis=0)
+            test_labels = np.concatenate((test_labels, dataset_dict['test_labels']), axis=0)
+    
+    np.save(os.path.join(dest_path, 'train_feature_matrix.npy'), train_matrix, allow_pickle=True)
+    np.save(os.path.join(dest_path, 'train_label.npy'), train_labels, allow_pickle=True)
+
+    np.save(os.path.join(dest_path, 'test_feature_matrix.npy'), test_matrix, allow_pickle=True)
+    np.save(os.path.join(dest_path, 'test_label.npy'), test_labels, allow_pickle=True)
+
+
+def patch_data_from_npz(folder_path, dest_path):
+    raise RuntimeError("Patch it mannually")
