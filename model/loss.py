52d84e094de22bad34aa304ab5be69d4c089846c
adrian <77871407@qq.com>
2020-12-07 19:15:39 -0600

    cos loss function added

--- model/loss.py
+++ model/loss.py
@@ -4,7 +4,14 @@ def average_difference_loss(output, label, weights):
     '''
     Simple averaged squared loss between labels
     '''
-    test = (label - output)*weights
+    loss = torch.mean(((label - output)*weights)**2)
+
+    return loss
+
+def cos_difference_loss(output, label, weights):
+    label[:, 0:2] = torch.cos(label[:, 0:2])
+    output[:, 0:2] = torch.cos(output[:, 0:2])
+
     loss = torch.mean(((label - output)*weights)**2)
 
     return loss
